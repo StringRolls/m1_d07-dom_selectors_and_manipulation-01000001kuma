3b58b59e7753b47e8ea382057f86d7479d9a4811
01000001kuma <a_illera@yahoo.com>
2022-04-21 13:29:06 +0200

    class

--- 02_class-dom_manipulation/scripts/script.js
+++ 02_class-dom_manipulation/scripts/script.js
@@ -1,1 +1,16 @@
-console.log("JS Loaded")
+console.log("JS Loaded in the client");
+
+const userInput = document.getElementById("to-do-item");
+const todoMainList = document.getElementById("items-list");
+
+userInput.addEventListener("change", (event) => {
+  // 1. Create the element
+  const newTodoElement = document.createElement("li");
+
+  // 2. fill out the porperties and the text
+  newTodoElement.textContent = event.target.value;
+  newTodoElement.classList.add("todoitem");
+
+  // 3. make it appear in the DOM by chosing a parent and insertin it into it
+  todoMainList.appendChild(newTodoElement); // .insertBefore()
+});
