4a300bc8aac59fd4a984be83b5001ddc1ad15893
01000001kuma <a_illera@yahoo.com>
2022-04-21 13:43:40 +0200

    class

--- 02_class-dom_manipulation/scripts/script.js
+++ 02_class-dom_manipulation/scripts/script.js
@@ -11,6 +11,16 @@ userInput.addEventListener("change", (event) => {
   newTodoElement.textContent = event.target.value;
   newTodoElement.classList.add("todoitem");
 
-  // 3. make it appear in the DOM by chosing a parent and insertin it into it
+  // 3. make it appear in the DOM by choosing a parent and inserting it into it
   todoMainList.appendChild(newTodoElement); // .insertBefore()
 });
+
+function handleClearAll(event) {
+  // A trick in HTML is you can have the first and last child from a parent
+  while (todoMainList.firstChild)
+    todoMainList.removeChild(todoMainList.lastChild);
+}
+
+const clearButton = document.getElementById("all-done");
+
+clearButton.addEventListener("click", handleClearAll);
